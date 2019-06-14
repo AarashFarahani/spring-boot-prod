4d6ffcd73bc4b6b49dac7b5dfab785aefc3cb0d7
Arash <arashfarahani@hotmail.com>
2019-06-14 12:23:59 +0430

    Initialized

---
+++ src/main/java/com/example/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import com.example.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(1,
+                String.format(template, name));
+    }
+}
